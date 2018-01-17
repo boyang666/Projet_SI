085f7c03086d390e0807edccb5ea00c8e550cdf4
ZHAOKaixing <zhaokaixing@gmail.com>
2018-01-17 15:55:48 +0100

    Update algo_flots_optiques.py
    Add comments

--- Projet_SI/algo_flots_optiques.py
+++ Projet_SI/algo_flots_optiques.py
@@ -4,26 +4,34 @@ import math
 import ZoneInteret as zi
 import algo
 
-
+#Classe qui implemente l'algorithme Flot Optique
 class flot_optiques(algo.algorithme):
+    #Retourner le nom d'algorithme
     def get_nomAlgo(self):
         return "Flot optiques"
 
+    #Fonction pricipale pour traiter la video
     def traiterVideo(self, video, start_frame):
         ma_liste = list()
         cap = cv2.VideoCapture(video)
+        
+        #Params pour ShiTomasi Corner Detection
         feature_params = dict(maxCorners=100, qualityLevel=0.3,
                               minDistance=7, blockSize=7)
+        
+        #Parameters for lucas kanade optical flow
         lk_params = dict(winSize=(15, 15), maxLevel=2,
                          criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-
+        
+        #Creer des couleurs aleatoire
         color = np.random.randint(0, 255, (100, 3))
         for i in range(0, start_frame):
             cap.read()
 
         ret, old_frame = cap.read()
         ret, frame = cap.read()
-
+        
+        #Fixer la detection dans la zone interet
         if zi.ZoneInteret.verifier_presence_fichier_ini():
             with open("./zi/param.ini", "r") as file:
                 line = file.readline()
@@ -40,14 +48,17 @@ class flot_optiques(algo.algorithme):
             #ret, frame = cap.read()
             if ret:
                 frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+                
+                #Calculer flot optique
                 p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
                 if p1 is None:
                     break
+                #Selectionner les bons points
                 good_new = p1[st == 1]
                 good_old = p0[st == 1]
 
                 norme = 0
+                #Faire le dessin qui indique le changement de points caracteristiques
                 for i, (new, old) in enumerate(zip(good_new, good_old)):
                     a, b = new.ravel()
                     c, d = old.ravel()
@@ -63,7 +74,8 @@ class flot_optiques(algo.algorithme):
                 k = cv2.waitKey(30) & 0xff
                 if k == 27:
                     break
-
+                
+                #Mise a jours le frame actuel
                 old_gray = frame_gray.copy()
                 p0 = good_new.reshape(-1, 1, 2)
 
